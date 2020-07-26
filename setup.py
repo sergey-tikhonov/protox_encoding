cc54983a5fc40229ea0ee163a503d6ba06000c7f
Sergey Tikhonov <srg.tikhonov@gmail.com>
2020-07-26 03:30:17 +0300

    Updated setup.py description

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ except ModuleNotFoundError:
 setup(
     name='protox_encoding',
     version='0.0.1',
+    url='http://github.com/sergey-tikhonov/protox_encoding',
     description='Protox encoding library written in Cython',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     ext_modules=cythonize('protox_encoding.pyx'),
     author='Sergey Tikhonov',
     author_email='srg.tikhonov@gmail.com',
